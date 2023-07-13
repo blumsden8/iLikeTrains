4e3dd0c671cca2dd0673cee2289717e7d38a6472
blumsden8 <131030947+blumsden8@users.noreply.github.com>
2023-07-13 14:41:22 -0400

    Create obstacle_detection.ino
    Very high level and basic start to obstacle detection. I still do not know how to use the sensors in the code.

---
+++ obstacle_detection.ino
@@ -0,0 +1,23 @@
+/*
+  bot is expected to:
+  - stop ~ 10 cm away from the obstacle
+  - turn around 180 degrees
+  - move back to a point where it can follow the line around the obstacle?
+  - record locations of obstacles?
+*/
+
+bool obstacle_is_close()
+{
+  /*if (obstacle > 10 cm away)
+  {
+    return false;
+  }
+  return true; // this function returning true should stop the robot because the obstacle is close
+  */
+}
+
+void turn_around()
+{
+  // this function should call a turning function that I believe Ozan made
+  //"bot.turn(180)" // we pass 180 as the amount of degrees the bot should turn so as to face away from the obstacle
+}
