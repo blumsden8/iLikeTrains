36a17f17840a86c2c68b15cddc58dc0f680b61af
blumsden8 <131030947+blumsden8@users.noreply.github.com>
2023-07-13 15:43:16 -0400

    Update obstacle_detection.ino
    could use some pointers as to whether or not I am on the right track with how to use these IR sensors from within the code

--- team13_bot/obstacle_detection.ino
+++ team13_bot/obstacle_detection.ino
@@ -8,16 +8,18 @@
 
 bool obstacle_is_close()
 {
-  /*if (obstacle > 10 cm away)
+  if (LEFT_IR_SENSOR > 10  || RIGHT_IR_SENSOR > 10 // cm away)
   {
     return false;
   }
   return true; // this function returning true should stop the robot because the obstacle is close
-  */
+  
 }
 
 void turn_around()
 {
   // this function should call a turning function that I believe Ozan made
+  bot.allStop();
+  bot.turnRight(180); // degrees
   //"bot.turn(180)" // we pass 180 as the amount of degrees the bot should turn so as to face away from the obstacle
 }
